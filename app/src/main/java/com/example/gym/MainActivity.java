2faafc0e017321239115b65d4c0c85be7247b92f
macbook <20522125@gm.uit.edu.vn>
2023-05-08 10:27:47 +0700

    .

--- app/src/main/java/com/example/gym/MainActivity.java
+++ app/src/main/java/com/example/gym/MainActivity.java
@@ -1,50 +1,115 @@
 package com.example.gym;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
+import android.annotation.SuppressLint;
+import android.media.Image;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-import java.util.ArrayList;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-    static ListView listView;
-    static ListViewAdapter adapter;
+import java.io.Console;
 
-    static Context context;
-    static ArrayList<String> name;
-    static ArrayList<String> time;
-//    ArrayAdapter<String> adapter;
-
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+    private LinearLayout llNameContainer, llAddressContainer, llParentContainer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // page message, group chat, post, chat
-        setContentView(R.layout.admin_dashboard);
-
-//        setContentView(R.layout.group_chat);
-
-//        setContentView(R.layout.post);
-
-//        setContentView(R.layout.chat);
+        getSupportActionBar().hide();
+        setContentView(R.layout.login);
 
 
-//        listView = findViewById(R.id.listview);
-//        name = new ArrayList<>();
-//        name.add("Tuan");
-//        name.add("Thanh");
-//        name.add("Nghia");
-//
-//        time = new ArrayList<>();
-//        time.add("2s ago");
-//        time.add("1 minute");
-//        time.add("5 minute");
-//
-//        adapter = new ListViewAdapter(this, name, time);
-//        listView.setAdapter(adapter);
+        redirection();
+    }
+
+    public void redirection() {
+        Button myButton = (Button) findViewById(R.id.buttonLogin);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Bạn đã nhấn vào button", Toast.LENGTH_SHORT).show();
+                setContentView(R.layout.item_user);
+            }
+        });
+
+        BottomNavigationView btNav = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        System.out.print("btNav");
+        System.out.print(btNav);
+        if (btNav != null) {
+        btNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                switch (id) {
+                    case R.id.users:
+                        setContentView(R.layout.item_user);
+                        return true;
+
+                    case R.id.courses:
+                        setContentView(R.layout.caloriestracking);
+                        return true;
+
+                    case R.id.post:
+                        setContentView(R.layout.feeds);
+                        return true;
+
+                    case R.id.order:
+                        setContentView(R.layout.payment);
+                        return true;
+                    default:
+                        return true;
+                }
+            }
+        });}
+    }
+    private void updateNavigationBarState(int actionId){
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+
+        for (int i = 0, size = menu.size(); i < size; i++) {
+            MenuItem item = menu.getItem(i);
+            System.out.print(item);
+            item.setChecked(item.getItemId() == actionId);
+        }
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.users:
+                setContentView(R.layout.item_user);
+                return true;
+
+            case R.id.courses:
+                setContentView(R.layout.caloriestracking);
+                return true;
+
+            case R.id.post:
+                setContentView(R.layout.feeds);
+                return true;
+
+            case R.id.order:
+                setContentView(R.layout.payment);
+                return true;
+            default:
+                setContentView(R.layout.item_user);
+                return true;
+
+        }
     }
 }
